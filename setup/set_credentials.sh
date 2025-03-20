896d0f4f9dcf5c88152d2173aaa2ea8947c1bf12
Rimsha Bashir <bashirrimsha22@gmail.com>
2025-03-20 17:10:07 +0100

    check to see if .env file is found or not

--- setup/set_credentials.sh
+++ setup/set_credentials.sh
@@ -3,7 +3,19 @@
 # KEY_PATH - full path of the SERVICE ACCOUNT CREDENTIAL file. 
 # To ensure that credentials are set up automatically every time you start a new shell session on your VM.
 
-source GHG-Emissions-Analytics-Pipeline/.env
+set -a  
+
+# Adjust the path to match your repo location
+ENV_FILE="$HOME/GHG-Emissions-Analytics-Pipeline/.env"
+
+if [ -f "$ENV_FILE" ]; then
+    source "$ENV_FILE"
+else
+    echo "Error: .env file not found at $ENV_FILE"
+    exit 1
+fi
+
+set +a
 
 # Add the environment variable GOOGLE_APPLICATION_CREDENTIALS to the .bashrc file
 echo "export GOOGLE_APPLICATION_CREDENTIALS=${KEY_PATH}" >> ~/.bashrc
