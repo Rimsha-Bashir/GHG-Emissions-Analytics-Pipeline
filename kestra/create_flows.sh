7ca2fbbe43b451a05482b02aae66f248385fbf72
Rimsha Bashir <bashirrimsha22@gmail.com>
2025-03-24 16:16:47 +0100

    fixed KV api call

--- kestra/create_flows.sh
+++ kestra/create_flows.sh
@@ -3,21 +3,18 @@
 # Source environment variables from .env file
 source .env
 
-# Set the URL for the Kestra API
-KESTRA_URL="http://${VM_IP}:${KESTRA_PORT}/api/v1/flows"
-
 # Post the flow to Kestra
 echo "Creating the flow in Kestra..."
-curl -v POST "$KESTRA_URL" \
+curl -v POST "http://${VM_IP}:${KESTRA_PORT}/api/v1/flows" \
     -H "Content-Type: application/x-yaml" \
     -u "${KESTRA_EMAIL}:${KESTRA_PASSWORD}" \
     --data-binary @gcp_kv.yml
 
 # Check if the flow creation was successful
 if [ $? -eq 0 ]; then
-    echo "Flow created successfully."
+    echo "----------------Flow created successfully---------------------"
 else
-    echo "Error creating the flow."
+    echo "!!!------Error creating the flow."
     exit 1
 fi
 
@@ -27,15 +24,16 @@ GCP_CREDS_FILE="~/.gc/ghg-creds.json"
 
 echo "Setting Key-Value pair in Kestra..."
 
-curl -v PUT "${KESTRA_URL}/kv/GCP_CREDS" \
-    -H "Content-Type: application/json" \
-    -u "${KESTRA_EMAIL}:${KESTRA_PASSWORD}" \
-    --data-binary @"$GCP_CREDS_FILE"
+curl -v PUT "http://$VM_IP:$KESTRA_PORT/api/v1/namespaces/$NAMESPACE/kv/GCP_CREDS" \
+     -H "Content-Type: application/json" \
+     -u "${KESTRA_EMAIL}:${KESTRA_PASSWORD}" \
+     --data-binary @"$GCP_CREDS_FILE"
+
 
 # Check if the KV pair was set successfully
 if [ $? -eq 0 ]; then
-    echo "Key-Value pair set successfully."
+    echo "----------------Key-Value pair set successfully----------------"
 else
-    echo "Error setting Key-Value pair."
+    echo "!!!------Error setting Key-Value pair."
     exit 1
 fi
