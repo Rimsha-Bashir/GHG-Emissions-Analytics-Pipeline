ce29a28c1c66bbd6a196c2b57643012727bfb31d
Rimsha Bashir <bashirrimsha22@gmail.com>
2025-03-24 16:27:34 +0100

    test

--- kestra/create_flows.sh
+++ kestra/create_flows.sh
@@ -4,7 +4,7 @@
 source .env
 
 # Post the flow to Kestra
-echo "Creating the flow in Kestra..."
+echo "Creating the gcp_kv.yml flow in Kestra..."
 curl -v POST "http://${VM_IP}:${KESTRA_PORT}/api/v1/flows" \
     -H "Content-Type: application/x-yaml" \
     -u "${KESTRA_EMAIL}:${KESTRA_PASSWORD}" \
@@ -12,28 +12,44 @@ curl -v POST "http://${VM_IP}:${KESTRA_PORT}/api/v1/flows" \
 
 # Check if the flow creation was successful
 if [ $? -eq 0 ]; then
-    echo "----------------Flow created successfully---------------------"
+    echo "----------------gcp_kv.yml created successfully---------------------"
 else
-    echo "!!!------Error creating the flow."
+    echo "!!!------Error creating the flow gcp_kv.yml."
     exit 1
 fi
 
-# Set a Key-Value pair (for example, GCP_CREDS) in Kestra
-# The JSON file for the key-value pair should be passed as a parameter
-GCP_CREDS_FILE="$HOME/.gc/ghg-creds.json"  # Correct the file path
 
-echo "Setting Key-Value pair in Kestra..."
+
+echo "Executing the flow gcp_kv.yml in Kestra..."
+EXECUTE_FLOW_RESPONSE=$(curl -s -X POST "http://${VM_IP}:${KESTRA_PORT}/api/v1/flows/gcp_kv/run" \
+    -H "Content-Type: application/json" \
+    -u "${KESTRA_EMAIL}:${KESTRA_PASSWORD}")
+
+# Check if the flow execution was successful
+if echo "$EXECUTE_FLOW_RESPONSE" | grep -q "status\":\"SUCCESS\""; then
+    echo "----------------gcp_kv.yml executed successfully--------------------"
+else
+    echo "!!!------Error executing the flow gcp_kv.yml."
+    exit 1
+fi
+
+
+
+# Set a Key-Value pair (for example, GCP_CREDS) in Kestra
+GCP_CREDS_FILE="$HOME/.gc/ghg-creds.json"  
+
+echo "Setting GCP_CREDS KV Pair in Kestra..."
 
 # Use PUT method to set the key-value pair
 curl -v -X PUT "http://$VM_IP:$KESTRA_PORT/api/v1/namespaces/$NAMESPACE/kv/GCP_CREDS" \
      -H "Content-Type: application/json" \
      -u "${KESTRA_EMAIL}:${KESTRA_PASSWORD}" \
-     --data-binary @"$GCP_CREDS_FILE"  # Correct the file path
+     --data-binary @"$GCP_CREDS_FILE"  
 
 # Check if the KV pair was set successfully
 if [ $? -eq 0 ]; then
-    echo "----------------Key-Value pair set successfully----------------"
+    echo "----------------GCP_CREDS set successfully----------------"
 else
-    echo "!!!------Error setting Key-Value pair."
+    echo "!!!------Error setting Key-Value pair GCP_CREDS."
     exit 1
 fi
