4b4728b1501758c9c8498ec345ce59be6715c659
Rimsha-Bashir <bashirrimsha22@gmail.com>
2025-04-01 15:27:43 +0000

    updated transform script

--- dbt_project/models/core/global_temp.sql
+++ dbt_project/models/core/global_temp.sql
@@ -10,8 +10,18 @@ SELECT
     year,
     SUM(total_co2) AS global_co2_emissions,
     SUM(total_ghg) AS global_ghg_emissions,
-    SUM(temperature_change_from_co2) AS co2_temp_change,
-    SUM(temperature_change_from_ch4) AS methane_temp_change,
-    SUM(temperature_change_from_n2o) AS n2o_temp_change
+
+    AVG(temperature_change_from_co2) AS co2_temp_change,
+    MIN(temperature_change_from_co2) as min_co2_temp_change, 
+    MAX(temperature_change_from_co2) as max_co2_temp_change,
+    
+    AVG(temperature_change_from_ch4) AS methane_temp_change,
+    MAX(temperature_change_from_ch4) AS max_methane_temp_change,
+    MIN(temperature_change_from_ch4) AS min_methane_temp_change,
+
+    SUM(temperature_change_from_n2o) AS n2o_temp_change,
+    MIN(temperature_change_from_n2o) AS min_n2o_temp_change,
+    MAX(temperature_change_from_n2o) AS max_n2o_temp_change
+
 FROM {{ ref('fact_emissions') }}
 GROUP BY year
